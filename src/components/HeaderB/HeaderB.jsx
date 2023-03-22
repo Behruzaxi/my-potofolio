c8e5806a1f43356d940f4b2e6e3713778720e2ce
Behruzaxi <ebehruz846@gmail.com>
2023-03-22 15:17:12 +0500

    hgfd

--- src/components/HeaderB/HeaderB.jsx
+++ src/components/HeaderB/HeaderB.jsx
@@ -12,33 +12,39 @@ export default function HeaderB() {
                 <Row>
                     <h5 className='work'>Featured works</h5>
                     <Col xs={12} lg={12} className='d-block d-sm-none'>
-                    <img className='one' src={bir} alt=""/>
-                    <h3 id='weather-responsive'>Weather project</h3>
-                    <div id='date-info'>
-                    <button id='date'><a href=""></a>Look</button>
-                    <a href="" id='github-link'>GitHub</a>
-                    </div>
-                    <p id='info-responsive'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia <br /> consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                        <img className='one' src={bir} alt="" />
+                        <h3 id='weather-responsive'>Weather project</h3>
+                        <div id='date-info'>
+                            <a href="https://joyful-maamoul-1cf1b4.netlify.app/">
+                                <button id='date'>Look</button>
+                            </a>
+                            <a href="https://github.com/Behruzaxi/weather-04" id='github-link'>GitHub</a>
+                        </div>
+                        <p id='info-responsive'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia <br /> consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
 
                     </Col>
                     <Col xs={12} lg={12} className='d-block d-sm-none'>
-                    <img className='one' src={bir} alt=""/>
-                    <h3 id='weather-responsive'>Weather project</h3>
-                    <div id='date-info'>
-                    <button id='date'><a href=""></a>Look</button>
-                    <a href="" id='github-link'>GitHub</a>
-                    </div>
-                    <p id='info-responsive'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia <br /> consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                        <img className='one' src={bir} alt="" />
+                        {/* <h3 id='weather-responsive'></h3> */}
+                        <div id='date-info'>
+                            <a href="https://stellar-crumble-a0176e.netlify.app/'>">
+                                <button id='date'>Look</button>
+                            </a>
+                            <a href="https://github.com/Behruzaxi/bekhruz-crud" id='github-link'>GitHub</a>
+                        </div>
+                        <p id='info-responsive'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia <br /> consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
 
                     </Col>
                     <Col xs={12} lg={12} className='d-block d-sm-none'>
-                    <img className='one' src={bir} alt=""/>
-                    <h3 id='weather-responsive'>Weather project</h3>
-                    <div id='date-info'>
-                    <button id='date'><a href=""></a>Look</button>
-                    <a href="" id='github-link'>GitHub</a>
-                    </div>
-                    <p id='info-responsive'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia <br /> consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                        <img className='one' src={bir} alt="" />
+                        <h3 id='weather-responsive'>Dynmic buttons</h3>
+                        <div id='date-info'>
+                            <a href="https://courageous-bombolone-a5a7c4.netlify.app/">
+                                <button id='date'>Look</button>
+                            </a>
+                            <a href="https://github.com/Behruzaxi/user-crud" id='github-link'>GitHub</a>
+                        </div>
+                        <p id='info-responsive'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia <br /> consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
 
                     </Col>
 
@@ -47,7 +53,7 @@ export default function HeaderB() {
 
 
                     <Col xs={5} lg={10} className='d-none d-sm-block'>
-                        <img className='one' src={bir} alt=""/>
+                        <img className='one' src={bir} alt="" />
                         <h3 className='des'>Weather project</h3>
                         <br></br>
                         <a href='https://joyful-maamoul-1cf1b4.netlify.app/'>
@@ -62,7 +68,7 @@ export default function HeaderB() {
                 </Row>
                 <Row>
                     <Col xs={5} lg={10} className='d-none d-sm-block'>
-                        <img className='one' src={ikki}  alt=""/>
+                        <img className='one' src={ikki} alt="" />
                         <h3 className='des'>Create Project</h3>
                         <br></br>
                         <a href='https://stellar-crumble-a0176e.netlify.app/'>
@@ -77,7 +83,7 @@ export default function HeaderB() {
                 </Row>
                 <Row>
                     <Col xs={5} lg={10} className='d-none d-sm-block'>
-                        <img className='one'  src={uch}  alt=""/>
+                        <img className='one' src={uch} alt="" />
                         <h3 className='des'>Dynamic Buttons Project</h3>
                         <br></br>
                         <a href='https://courageous-bombolone-a5a7c4.netlify.app/'>
